b3db8386fd64fbc1bf010fce301756fe72b117a6
Skylar <syema.ailia@gmail.com>
2018-02-06 15:26:57 -0600

    search google maps in terminal using py

---
+++ webscraping.py
@@ -0,0 +1,9 @@
+import webbrowser, sys 
+
+#address = '5500 St Louis Ave, Chicago, IL 60625'
+
+if (len(sys.argv) > 1):
+	# get address from command line
+	address = ' '.join(sys.argv[1:])
+
+webbrowser.open('https://www.google.com/maps/search/' + address)
